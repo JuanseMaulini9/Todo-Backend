653bd54e7ea53d69e0ca5087e5b3d7425ba024a3
juansemaulini9 <juansesosa45@gmail.com>
2024-06-05 16:19:40 -0300

    singup & loggin

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -3,25 +3,60 @@ import bcrypt from "bcrypt";
 import User from "../schemas/user.schema";
 
 export const singup = async (req: Request, res: Response) => {
-  const { username, password } = req.body;
+  try {
+    const { username, password } = req.body;
 
-  if (typeof username === "string" && typeof password === "string") {
-    const hashedPassword = await bcrypt.hash(password, 10);
-    const newUser = new User({
-      username,
-      password: hashedPassword,
-    });
-    if (newUser) {
-      await newUser.save();
-      res.status(201).send("Usuario creado exitosamente");
+    if (typeof username === "string" && typeof password === "string") {
+      const userExist = await User.findOne({ username });
+
+      if (userExist) {
+        return res.status(400).send("el usuario ya existe");
+      }
+
+      const hashedPassword = await bcrypt.hash(password, 10);
+      const newUser = new User({
+        username,
+        password: hashedPassword,
+      });
+      if (newUser) {
+        await newUser.save();
+        res.status(201).send("Usuario creado exitosamente");
+      }
+    } else {
+      res.status(500).send("Datos invalidos");
     }
-  } else {
-    res.status(500).send("Datos invalidos");
+  } catch (error) {
+    if (error instanceof Error) {
+      return res.status(500).send(`Ìnternal Error: ${error.message}`);
+    }
   }
 };
 
-export const login = (req: Request, res: Response) => {
-  res.send("soy el login");
+export const login = async (req: Request, res: Response) => {
+  try {
+    const { username, password } = req.body;
+    if (typeof username !== "string" && typeof password !== "string") {
+      return res.status(400).send("Datos no validos");
+    }
+
+    const user = await User.findOne({ username });
+
+    if (!user) {
+      return res.status(400).send("Usuario no registrad");
+    }
+
+    const correctPassword = await bcrypt.compare(password, user.password);
+    if (!correctPassword) {
+      return res.status(400).send("Datos no validos");
+    }
+
+    return res.status(200).send(`${user?.username} logged`);
+  } catch (error) {
+    if (error instanceof Error) {
+      return res.status(500).send(`Internal Error: ${error.message}`);
+    }
+    return res.status(500).send(error);
+  }
 };
 export const logout = (req: Request, res: Response) => {
   res.send("soy el logout");
