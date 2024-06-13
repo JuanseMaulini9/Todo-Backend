ec929cdb06c6211376de848616039c1f24cc7199
juansemaulini9 <juansesosa45@gmail.com>
2024-06-13 15:34:48 -0300

    delete & put

--- src/controllers/todo.controller.ts
+++ src/controllers/todo.controller.ts
@@ -1,4 +1,4 @@
-import { Request, Response } from "express";
+import { Request, Response, response } from "express";
 import Todo from "../schemas/todo.schema";
 
 export const getTodo = async (req: Request, res: Response) => {
@@ -35,5 +35,37 @@ export const createTodo = async (req: Request, res: Response) => {
     }
   }
 };
-export const deleteTodo = (req: Request, res: Response) => {};
-export const editTodo = (req: Request, res: Response) => {};
+export const deleteTodo = async (req: Request, res: Response) => {
+  try {
+    const { _id } = req.body;
+    if (typeof _id !== "string") {
+      return res.status(400).send("Error type");
+    }
+    await Todo.findOneAndDelete({ _id });
+    res.status(201).send("todo ok");
+  } catch (error) {
+    if (error instanceof Error) {
+      res.send(error);
+    }
+  }
+};
+export const editTodo = async (req: Request, res: Response) => {
+  try {
+    const { title, content, expires, value, _id } = req.body;
+    const updatedTodo = await Todo.findByIdAndUpdate(
+      _id,
+      {
+        title,
+        content,
+        expires,
+        value,
+      },
+      { new: true }
+    );
+    res.status(200).send(`Todo actualizada: ${updatedTodo}`);
+  } catch (error) {
+    if (error instanceof Error) {
+      res.send(error);
+    }
+  }
+};
